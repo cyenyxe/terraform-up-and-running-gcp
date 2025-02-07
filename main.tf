9e8a2881c756b0ab14798a39f7426812e337fb56
Cristina Yenyxe Gonzalez Garcia <c.gonzalez@eyeo.com>
2025-02-07 11:30:23 +0100

    Exposed port 8080 in VM instance

--- main.tf
+++ main.tf
@@ -6,19 +6,28 @@ provider "google" {
 
 resource "google_service_account" "default" {
   account_id   = "vm-instance-sa"
-  display_name = "Custom SA for VM instance"
+  display_name = "Custom SA for web server VM instance"
 }
 
 resource "google_compute_instance" "default" {
-  name         = "vm-instance"
+  name         = "web-server"
   machine_type = "e2-micro"
+  tags         = ["web"]
   labels = {
     "chapter" = "2"
   }
 
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    echo "hello world" > index.html
+    nohup busybox httpd -f -p 8080 &
+  EOF
+
+  allow_stopping_for_update = true
+
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
       labels = {
         sample-label = "sample-value"
       }
@@ -27,5 +36,14 @@ resource "google_compute_instance" "default" {
 
   network_interface {
     network = "default"
+
+    access_config {
+
+    }
+  }
+
+  service_account {
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
   }
 }
